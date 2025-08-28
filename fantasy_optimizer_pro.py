65a9324d1be4d89e349a5625300daf34383a27e9
rchiplock <rchiplock@gmail.com>
2025-08-28 07:00:33 -0400

    Update fantasy_optimizer_pro.py

--- fantasy_optimizer_pro.py
+++ fantasy_optimizer_pro.py
@@ -141,8 +141,8 @@ def calc_multiplier(row, spread_trigger, total_trigger, impact_pct):
 st.title("🏈 DFS Fantasy Football Optimizer Pro")
 
 st.write("---")
-st.markdown(["Retrieve DraftKings Salary File 🏆](https://www.draftkings.com/home/contestlistbysport?sport=NFL")
-st.markdown(["Retrieve FanDuel Salary File 🏆](https://www.fanduel.com/contests/nfl/5387")
+st.markdown("[Retrieve DraftKings Salary File 🏆](https://www.draftkings.com/home/contestlistbysport?sport=NFL")
+st.markdown("[Retrieve FanDuel Salary File 🏆](https://www.fanduel.com/contests/nfl/5387")
 
 
 num_lineups=st.sidebar.number_input("Number of Lineups",1,20,3,help="How many lineups should the optimizer build?")
@@ -342,5 +342,6 @@ if st.session_state.optimizer_started and salary_file:
 
 st.write("---")
 st.markdown("Enjoying the App? [Visit GitHub](https://github.com/rchiplock/fantasy-optimizer) for the latest updates or to contribute!")
+
 
 
